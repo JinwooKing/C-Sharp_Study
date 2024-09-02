5aac0b0577fc67fc5381d6854a94d6c1ec0025ed
JinwooKing <wlsdn9489@naver.com>
2024-09-02 09:53:30 +0900

    update sorce

---
+++ Model/Helper/HttpRequestHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Dynamic;
+using System.Net;
+
+namespace HttpParser
+{
+    public class HttpRequestHelper
+    {
+        public enum ContentType
+        {
+            xml,
+            json
+        }
+
+        public enum Method
+        {
+            POST,
+            GET
+        }
+
+        public static string GetData(string requestUriString, ContentType contentType, Method method)
+        {
+            //1. HTTP 요청
+            var request = (HttpWebRequest)WebRequest.Create(requestUriString);
+            request.ContentType = "application/" + contentType.ToString();
+            request.Method = method.ToString();
+
+            //POST 요청에만 사용 가능
+            if (method == Method.POST)
+            {
+                var streamWriter = new StreamWriter(request.GetRequestStream());
+                streamWriter.Write("");
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            //2. Response 확인
+            var httpResponse = (HttpWebResponse)request.GetResponse();
+            var streamReader = new StreamReader(httpResponse.GetResponseStream());
+            return streamReader.ReadToEnd();
+        }
+    }
+}
---
+++ Study/Cls/XmlDocumentCls.cs
@@ -0,0 +1,21 @@
+﻿using HttpParser;
+using System.Xml.Linq;
+using static HttpParser.HttpRequestHelper;
+
+namespace ConsoleAppSample.Study.Cls
+{
+    public class XmlDocumentCls
+    {
+        public XmlDocumentCls()
+        {
+            var xmldata = HttpRequestHelper.GetData(@"", ContentType.xml, Method.GET);
+            XDocument doc = XDocument.Parse(xmldata);
+
+            var upperDamValue = doc.Descendants("item")
+                                   .Where(x => x.Element("expl").Value == "수위")
+                                   .Select(x => x.Element("value").Value)
+                                   .FirstOrDefault();
+            Console.WriteLine("수위의 value 값: " + upperDamValue);
+        }
+    }
+}
